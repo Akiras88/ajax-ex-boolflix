0be4fd452fe34aedb8c95d89481ecf02fd7137d3
Francesco <francesco.scarnera3@gmail.com>
2020-05-05 18:01:24 +0200

    to fix, milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,14 @@ $(document).ready(function() {
     // search with button and enter 
     btnSearch.click(function(){
         var query = inputSearch.val().trim();
-        printMovies(template, query, inputSearch);
+        searchMovies(template, query, inputSearch);
+        searchTv(template, query, inputSearch);
     });
     inputSearch.keypress(function(e) {
         if(e.which == 13) {
             var query = inputSearch.val().trim();
-            printMovies(template, query, inputSearch);
+            searchMovies(template, query, inputSearch);
+            searchTv(template, query, inputSearch);
         }
     });
 
@@ -39,8 +41,8 @@ $(document).ready(function() {
     FUNCTIONS
  **********************************************/
 
-// search function
-function printMovies(template, query, inputSearch) {
+// search movie function
+function searchMovies(template, query, inputSearch) {
     // reference
     var movies = $('.movies');
     // reference API
@@ -57,9 +59,8 @@ function printMovies(template, query, inputSearch) {
                 query: query
             },
             success: function(res) {
-                reset(movies);
                 var movieInfo = res.results;
-                if ( movieInfo.length > 0 ) {
+                if (( movieInfo.length > 0 ) || ( tvInfo.length > 0 )) {
                     printMovie(movieInfo, template, movies);
                 } else {
                     alert('Prego, inserisci una parola valida');
@@ -76,6 +77,43 @@ function printMovies(template, query, inputSearch) {
     }
 }
 
+// search tv function
+function searchTv(template, query, inputSearch) {
+    // reference
+    var movies = $('.movies');
+    // reference API
+    var apiKey = '3fd3d81771a2efd18bf7d6e160d4ad81';
+    var language = 'it-IT';
+    if ( query !== '' ) {
+        // call API
+        $.ajax ({
+            url : 'https://api.themoviedb.org/3/search/tv',
+            method : 'GET',
+            data : {
+                api_key : apiKey,
+                language : language,
+                query: query
+            },
+            success : function(res){
+                var tvInfo = res.results;
+                if ( tvInfo.length > 0 ) {
+                    printTv(tvInfo, template, movies);
+                } else {
+                    alert('Prego, inserisci una parola valida');
+                    inputSearch.select();
+                }
+            },
+            error: function() {
+                alert('Inserisci un titolo valido');
+                inputSearch.focus();
+            }
+        });
+    } else {
+        alert('Inserisci un titolo valido');
+        inputSearch.focus();
+    }
+}
+
 // print movie details function
 function printMovie(movieInfo, template, movies) {
     reset(movies);
@@ -85,10 +123,29 @@ function printMovie(movieInfo, template, movies) {
             title : movie.title,
             original_title : movie.original_title,
             original_language : flag(movie),
-            rating : ratingStar(movie)
+            rating : ratingStar(movie),
+            type : 'movie'
         }
     // add template
     var html = template(movieObj);
+    movies.append(html);
+    }
+}
+
+//print tv details function
+function printTv(tvInfo, template, movies) {
+    reset(movies);
+    for (var i = 0; i < tvInfo.length; i++) {
+        var tv = tvInfo[i];
+        var tvObj = { 
+            title : tv.name,
+            original_title : tv.original_name,
+            original_language : flag(tv),
+            rating : ratingStar(tv),
+            type : 'tv'
+        }
+    // add template
+    var html = template(tvObj);
     movies.append(html);
     }
 }
@@ -102,8 +159,8 @@ function reset(element) {
 function ratingStar(movie) {
     var ratingCeil = Math.ceil(movie.vote_average / 2);
     var star = '';
-    // for ( var i = 0; i > ratingCeil; i++) {
-        // star += '<i class="fas fa-star"></i>';
+    // for ( var k = 0; k > ratingCeil; k++) {
+        // star += '<i class="far fa-star"></i>';
     // }
     for ( var i = 0; i < ratingCeil; i++) {
         star += '<i class="fas fa-star"></i>';
